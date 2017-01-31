6c5d1f84c8c11107b96be05e287e7c3d9e852c53
Ahn Kyeong Chan <ahnkc@mbc.co.kr>
2017-01-31 17:02:47 +0900

    내용추가

---
+++ app/src/main/java/com/cgpink/photogallery/StartupReceiver.java
@@ -0,0 +1,20 @@
+package com.cgpink.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by pinkgom on 2017-01-31.
+ */
+
+public class StartupReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+    }
+}
